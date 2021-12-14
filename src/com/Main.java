ec79f40a5efe2d81cdde6360d65b4f5c0ce9ab93
Irina <irisha.kachur@icloud.com>
2021-12-14 12:07:15 +0300

    commit 3

--- src/com/Main.java
+++ src/com/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi!");
         System.out.println("data life");
+
+    }
+    public void start() {
+        System.out.println("Start");
+
     }
 }
